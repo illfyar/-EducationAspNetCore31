048d28a99406dbd5b5172ecd8b84cd217c0ba10f
ilya <illf.yar@googlemail.com>
2021-01-16 17:34:51 +0300

    add adication application

---
+++ WebApplicationEducation/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationEducation.Models;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WebApplicationEducation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View(GetAllUser());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private List<User> GetAllUser()
+        {
+            using (IDbConnection db = Connection)
+            {
+                List<User> result = db.Query<User>("SELECT * FROM Users").ToList();
+                return result;
+            }
+        }
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public DateTime Created { get; set; }
+        }
+    }
+}
